f7b21a01fa46849e3d1374a31b8c9f6932e48d3f
Rafael Peria de Sene <rpsene@br.ibm.com>
2016-10-05 14:35:33 -0300

    sdk-systemsim: add release parameter for build
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- releng/build.sh
+++ releng/build.sh
@@ -37,16 +37,21 @@
 #Creates a zip file which contains the set of files necessary to 
 #install and execute the IBM POWER Functional Simulator.
 
+if [ "$1" != "release" ]; then
+	echo
+else
+	#gets the date to be used as part of the .zip name
+	VAR=$(date +%m%d%Y%H%M%S | sed 's/\(:[0-9][0-9]\)[0-9]*$/\1/')
+fi
+
 #check if the required program exists.
 if which zip >/dev/null; then
 	#clear old zip files.
 	rm -f ./*.zip
 	mkdir ./sdk-systemsim
 	cp -rf ../packages/ ../README.md ../repos/ ../scripts/ ../setupsimulator ./sdk-systemsim
-	#gets the date to be used as part of the .zip name
-	VAR=$(date +%m%d%Y%H%M%S | sed 's/\(:[0-9][0-9]\)[0-9]*$/\1/')
 	#create the .zip file with the structure systemsim_$VAR.zip
-	zip -r systemsim_$VAR.zip ./sdk-systemsim
+	zip -r systemsim$VAR.zip ./sdk-systemsim
 	rm -rf ./sdk-systemsim
 else
     echo "Could not find zip program. Please install it and try again."
